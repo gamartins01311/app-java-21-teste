cad5fff4a40b760cdce0e6886b99c62a506c7214
gamartins01311 <gamartins013@gmail.com>
2025-04-03 20:38:56 -0300

    feat: codigo inicial app-teste

---
+++ src/test/java/app/java/_1/teste/ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.java._1.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
